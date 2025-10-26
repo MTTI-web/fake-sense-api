8101e634fd306bf0603a91d2b6007b1b537b58b4
MTTI-web <srijankumardps@gmail.com>
2025-10-26 11:23:29 +0530

    implemented a real model

--- sandbox.py
+++ sandbox.py
@@ -6,20 +6,43 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+import emoji
 
 df = pd.read_csv("./data/dataset.csv")
 df.head(3)
 
-X = df["text"].values
+X = df[["rating", "text"]]
 y = df["label"] == "CG"
 
 y = y.astype(int)
 
 y.values
+
+
+def emoji_to_text(text):
+    return emoji.demojize(text)
+
+
 # %%
-pipe = Pipeline(
-    [("tfidf", TfidfVectorizer()), ("classifier", LogisticRegression(C=0.1))]
+from sklearn.compose import ColumnTransformer
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("tfidf", TfidfVectorizer(preprocessor=emoji_to_text), "text"),
+        # 'passthrough' means the 'rating' column is not changed.
+        # You could also use a scaler like StandardScaler()
+        ("numeric", "passthrough", ["rating"]),
+    ]
 )
+
+# Create the full pipeline
+pipe = Pipeline(
+    [
+        ("preprocessor", preprocessor),
+        ("classifier", LogisticRegression(C=0.1, max_iter=1000)),
+    ]
+)
+
 
 pipe.fit(X, y)
 pred = pipe.predict(X)
@@ -72,7 +95,7 @@ plt.show()
 # %%
 from sklearn.calibration import calibration_curve
 
-prob_true, prob_pred = calibration_curve(y, g_pred[:, 1], n_bins=200)
+prob_true, prob_pred = calibration_curve(y, g_pred[:, 1], n_bins=10)
 plt.plot(prob_pred, prob_true, marker="o")
 plt.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
 plt.xlabel("Mean predicted probability")
